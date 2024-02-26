b1b1a89a3711703315270c88857e203116165d39
Owen.Liu <1571646680113305@mil>
2024-02-26 15:59:51 -0800

    adding the updated processing steps 1-5

---
+++ process steps/split raw VMS by year.R
@@ -0,0 +1,25 @@
+# helper file to chunk up the enormous vms file Jameal downloaded
+
+library(tidyverse)
+library(here)
+
+# vms_raw <- read_csv(here::here('data','raw','vms','vms_01012009_12312023.csv'))
+
+# wayyy too big, so we need to do this in pieces
+# try to brute force in 10M-row segments
+
+st <- 0
+chunk_num<-1
+
+#I'll keep running the following lines until we run out of data to parse
+# looks like 11 10M-row chunks
+
+x <- read_csv(here('data','raw','vms','vms_01012009_12312023.csv'),
+              skip=st,n_max=1e7,lazy=T,col_names = cn) %>% 
+  mutate(UTCDATETIME=parse_date_time(UTC_TIME, c("mdy HM","mdy HMS"))) %>% 
+  mutate(year=year(UTCDATETIME))
+cn <- names(x)
+yrs_captured <- paste(unique(x$year),collapse=" ")
+write_rds(x,here('data','raw','vms',paste0('vms chunk ',chunk_num," ",yrs_captured,'.rds')))
+st <- st+1e7
+chunk_num <- chunk_num+1
