9317e088f65249498a61cefa9d30e07c5130b0bb
Blake Feist <blake.feist@noaa.gov>
2024-05-10 14:40:17 -0700

    Update Blake misc code

--- Blake misc code/fish ticket related.R
+++ Blake misc code/fish ticket related.R
@@ -2,11 +2,10 @@
 # Blake Feist - 7 May 2024
 
 # packages
-library(knitr)
-library(here)
 library(dplyr)
-library(fredr)
 library(readr)
+library(desc)
+library(DescTools)
 
 # load master 1994 - 2023 fish tickets file
 all_fishtickets_1994_2023 <- readRDS("~/Documents/GitHub/VMS-pipeline/Confidential/raw/fish tickets/all_fishtickets_1994_2023.rds")
@@ -24,3 +23,23 @@ glimpse(Dungeness)
 # tally the number of tickets by PacFIN gear code to figure out which gear types to use
 DCRB_gear <- table(Dungeness$PACFIN_GEAR_CODE)
 # 99.73% of all DCRB tickets are from CPT, CLP, OTH or OPT, so only select those in master_process script
+
+
+# filter only Chinook salmon tickets
+Chinook <- all_fishtickets_1994_2023 %>%
+  filter(PACFIN_SPECIES_CODE == "CHNK")
+
+# take a look at the Chinook salmon attribute structure
+glimpse(Chinook)
+
+# tally the number of tickets by PacFIN gear code to figure out which gear types to use
+CHNK_gear <- table(Chinook$PACFIN_GEAR_CODE)
+# more detailed tally the number of tickets by PacFIN gear code to figure out which gear types to use
+CHNK_gear <- Freq(Chinook$PACFIN_GEAR_CODE, ord-"desc")
+
+# 99.88% of all CHNK tickets are from "TRL","GLN","STN","DPN","MDT","SEN","POL","JIG","ONT", so only select those in master_process script
+
+## for running Chinook using master_process.Rmd
+spp_codes <- c("CHNK")
+
+gear_codes <- c("TRL","GLN","STN","DPN","MDT","SEN","POL","JIG","ONT")
