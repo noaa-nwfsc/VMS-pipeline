5bc9656aa8d88bc9a3910149a5573bb97eca76e3
Owen R. Liu <owen.liu@noaa.gov>
2025-02-26 09:16:57 -0800

    minor file path change

--- code/notebooks/owen/split raw VMS by year.R
+++ code/notebooks/owen/split raw VMS by year.R
@@ -13,16 +13,16 @@ chunk_num<-1
 
 #I'll keep running the following lines until we run out of data to parse
 # looks like 11 10M-row chunks
-cn <- c("DOCNUM","VESSEL_NAME","DECLARATION_CODE","UTC_TIME","COURSE","SPEED","AVG_COURSE","AVG_SPEED","LAT","LON","FMC_PROVIDER_ID","PROVIDER","DATA_LOAD_DATE","UTCDATETIME","year")
-x <- read_csv(here('Confidential','raw','vms','vms_01012009_12312023.csv'),
+cn <- c("DOCNUM","VESSEL_NAME","DECLARATION_CODE","UTC_TIME","COURSE","SPEED","AVG_COURSE","AVG_SPEED","LAT","LON","FMC_PROVIDER_ID","PROVIDER","DATA_LOAD_DATE")
+x <- read_csv(here('Confidential','data','raw','vms','vms_01012009_12312023.csv'),
               skip=st,n_max=1e7,lazy=T,
               # column names explicit
               col_names = cn,
               # column types explicit
-              col_types='ccicddddddicc') %>% 
+              col_types='ccicdddddddcc') %>% 
   mutate(UTCDATETIME=parse_date_time(UTC_TIME, c("mdy HM","mdy HMS"))) %>% 
   mutate(year=year(UTCDATETIME))
 yrs_captured <- paste(unique(x$year),collapse=" ")
-write_rds(x,here('Confidential','raw','vms',paste0('vms chunk ',chunk_num," ",yrs_captured,'.rds')))
+write_rds(x,here('Confidential','data','raw','vms',paste0('vms chunk ',chunk_num," ",yrs_captured,'.rds')))
 st <- st+1e7
 chunk_num <- chunk_num+1
